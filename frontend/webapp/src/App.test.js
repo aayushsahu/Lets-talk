f46196511143af1c10a50552986c3cb9edc90a01
SOLINVICTUS <ayush297@gmail.com>
2024-11-11 21:32:29 +0530

    feat: hooking pre-commit with lint-staged and test

--- frontend/webapp/src/App.test.js
+++ frontend/webapp/src/App.test.js
@@ -4,7 +4,7 @@ import { test, expect } from '@jest/globals';
 import React from 'react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders app and 1===1         ', () => {
   render(<App />);
   expect(1).toBe(1);
 });
--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -3,6 +3,5 @@
 
 echo "Woof-woof by husky! Running pre-commit hook"
 cd ./frontend/webapp/
-npm run lint:fix
-npm run format
+npm run lint-staged:fix
 npm run test
